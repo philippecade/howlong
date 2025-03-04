2aec19510c3173d6b96edce889469b54eb0ac0ca
XPHC <philippe.cade@swiss-as.com>
2025-03-04 11:28:15 +0100

    Refactor for manifest V3
    Also only show the dialog if on the time recording window.
    New version 0.5

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(activeTab) {
-    chrome.tabs.executeScript(null, { file: "jquery-3.4.1.min.js" },
-		function() {
-			chrome.tabs.executeScript(null, {file: "howlong.js"});
-		});
-});
